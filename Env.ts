650c36b1172166666a827f514ab7eac28a010f7a
Beshoy R Mansour <beshoy.r.mansour@gmail.com>
2024-08-23 10:05:06 +0300

    Add env file

---
+++ Env.ts
@@ -0,0 +1,8 @@
+// @ts-ignore
+import { BASE_URL, PACKUP_BASE_URL } from "@env";
+
+export default {
+  BASE_URL: BASE_URL,
+  PACKUP_BASE_URL: PACKUP_BASE_URL,
+  // BASE_URL: process.env.BASE_URL,
+};
---
+++ env.d.ts
@@ -0,0 +1,4 @@
+declare module '@env' {
+    export const BASE_URL: string;
+    export const PACKUP_BASE_URL: string;
+}
