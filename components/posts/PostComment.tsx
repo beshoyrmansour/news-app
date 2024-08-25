42d3022fd97630741496078fbf3abb29b788cebb
Beshoy R Mansour <beshoy.r.mansour@gmail.com>
2024-08-25 12:16:42 +0300

    use Post User in comment

--- components/posts/PostComment.tsx
+++ components/posts/PostComment.tsx
@@ -3,36 +3,44 @@ import React from "react";
 import { Comment } from "@/types/comments";
 import { ThemedView } from "../ThemedView";
 import { ThemedText } from "../ThemedText";
+import PostUser from "./PostUser";
 type Props = Comment
 const PostComment = ({
   body,
   email,
-  id,
   name,
   postId
 }: Props) => {
   return (
-      <ThemedView style={styles.container}>
-        <ThemedText type="subtitle">{name}</ThemedText>
-        <ThemedText>
-          {body}
-        </ThemedText>
-        
-        <ThemedText type="link">
-          {email}
-        </ThemedText>
-
+    <ThemedView style={styles.container}>
+      <ThemedText type="subtitle">{name}</ThemedText>
+      <ThemedView style={styles.user}>
+        <ThemedText>by</ThemedText>
+        <PostUser postId={postId.toString()} />
       </ThemedView>
+      <ThemedText>
+        {body}
+      </ThemedText>
+      <ThemedText type="link">
+        {email}
+      </ThemedText>
+
+    </ThemedView>
   );
 };
 
 export default PostComment;
 
 const styles = StyleSheet.create({
-    container: {
-    gap: 8,
+  container: {
+    gap: 1,
     marginBottom: 22,
-    marginRight:32,
-    marginLeft:32,
+    marginRight: 32,
+    marginLeft: 32,
   },
+  user: {
+    flexDirection: 'row',
+    alignItems: 'baseline',
+    gap: 3,
+  }
 });
